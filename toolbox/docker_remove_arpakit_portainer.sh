c8d3844882a9e9bcb1a7c5319da7ed68f819debc
arpakit <arpakit@gmail.com>
2025-11-15 07:56:13 +0500

    fix

---
+++ toolbox/docker_remove_arpakit_portainer.sh
@@ -0,0 +1,3 @@
+docker stop arpakit_portainer
+docker rm arpakit_portainer
+docker ps
--- toolbox/docker_run_arpakit_portainer.sh
+++ toolbox/docker_run_arpakit_portainer.sh
@@ -1,12 +1,12 @@
-docker pull portainer/portainer-ce:2.33.2
 docker stop arpakit_portainer
 docker rm arpakit_portainer
 docker run -d \
   -p 50533:8000 \
   -p 50534:9443 \
+  -p 50537:9000 \
   --name arpakit_portainer \
   --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v arpakit_portainer_volume:/data \
   portainer/portainer-ce:latest
-echo "web port: 50534"
+echo "http: 50534, https: 50537"
