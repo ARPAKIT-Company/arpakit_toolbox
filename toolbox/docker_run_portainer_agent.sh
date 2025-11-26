48e04da79a2dc4b8fad1e6617ed7807e6393e20b
arpakit <arpakit@gmail.com>
2025-11-26 19:17:32 +0500

    fix

--- toolbox/docker_run_portainer_agent.sh
+++ toolbox/docker_run_portainer_agent.sh
@@ -1,6 +1,6 @@
 docker run -d \
   -p 9001:9001 \
-  --name arpakit_portainer_agent \
+  --name portainer_agent \
   --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /var/lib/docker/volumes:/var/lib/docker/volumes \
--- toolbox/docker_run_portainer_ce_no_agent_port.sh
+++ toolbox/docker_run_portainer_ce_no_agent_port.sh
@@ -1,11 +1,10 @@
 docker stop portainer
 docker rm portainer
 docker run -d \
-  -p 8000:8000 \
   -p 9000:9000 \
   --name portainer \
   --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v portainer_volume:/data \
   portainer/portainer-ce:latest
-echo "http: 9000, for agents: 8000"
+echo "http: 9000"
