f3917fd75a35e3862a9bc18f6c3f6f72eddff387
arpakit <arpakit@gmail.com>
2025-11-26 20:05:34 +0500

    fix

--- toolbox/docker_run_portainer.sh
+++ toolbox/docker_run_portainer.sh
@@ -1,10 +1,12 @@
 docker stop portainer
 docker rm portainer
+docker pull portainer/portainer-ce:2.33.2
+
 docker run -d \
   -p 9000:9000 \
   --name portainer \
   --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v portainer_data:/data \
+  -v portainer_volume:/data \
   portainer/portainer-ce:latest
 echo "http: 9000"
