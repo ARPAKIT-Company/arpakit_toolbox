32f957c64de516934fc195f203fc8128c33fa35d
arpakit <arpakit@gmail.com>
2025-12-05 16:11:15 +0500

    fix

---
+++ toolbox/install_nginx.sh
@@ -0,0 +1,12 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt install nginx
+
+sudo systemctl enable nginx
+
+mkdir /etc/nginx/ssl
+
+sudo openssl dhparam -out /etc/nginx/ssl-dhparams.pem 2048
+
+sudo systemctl restart nginx
