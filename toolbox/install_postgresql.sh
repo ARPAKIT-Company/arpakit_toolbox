9e0160a99c897517b8ec457b29793c6aa2af92f0
arpakit <arpakit@gmail.com>
2025-12-07 03:47:29 +0500

    fix

--- toolbox/install_postgresql.sh
+++ toolbox/install_postgresql.sh
@@ -2,5 +2,5 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 sudo apt install postgresql postgresql-contrib
-$ sudo systemctl enable postgresql.service
+sudo systemctl enable postgresql.service
 sudo systemctl start postgresql.service
