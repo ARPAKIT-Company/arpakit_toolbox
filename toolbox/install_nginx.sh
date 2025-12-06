f338f51fc092d1495a1d4d42f88e4ae5d369fc7e
arpakit <arpakit@gmail.com>
2025-12-07 04:31:34 +0500

    fix

--- toolbox/install_nginx.sh
+++ toolbox/install_nginx.sh
@@ -7,6 +7,6 @@ sudo systemctl enable nginx
 
 mkdir /etc/nginx/ssl
 
-sudo openssl dhparam -out /etc/nginx/ssl-dhparams.pem 2048
+sudo openssl dhparam -out /etc/nginx/ssl/ssl-dhparams.pem 2048
 
 sudo systemctl restart nginx
