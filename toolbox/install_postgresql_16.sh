db45a89e560b8b3ff1e7ef51bc00c0a5aea3bb01
arpakit <arpakit@gmail.com>
2026-01-26 10:44:43 +0500

    fix

--- toolbox/install_postgresql_16.sh
+++ toolbox/install_postgresql_16.sh
@@ -3,6 +3,9 @@ sudo apt-get upgrade -y
 
 curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /usr/share/keyrings/postgresql.gpg
 echo "deb [signed-by=/usr/share/keyrings/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt noble-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+
+sudo apt update
+
 sudo apt install -y postgresql-16 postgresql-contrib-16
 
 
